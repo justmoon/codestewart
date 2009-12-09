cab66ed5f3fd1883f40af3734ef3e744c978fe11
Stefan Thomas <moon@justmoon.de>
2009-12-09 15:29:11 +0000

    Switched to dark hive theme.

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0/build/cssreset/reset-min.css"> 
 		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0/build/cssbase/base-min.css"> 
 		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0/build/cssfonts/fonts-min.css">
-		<link type="text/css" href="<?=cs_res_url('jqueryui/css/vader/jquery-ui-1.7.2.custom.css');?>" rel="stylesheet" />
+		<link type="text/css" href="<?=cs_res_url('jqueryui/css/dark-hive/jquery-ui-1.7.2.custom.css');?>" rel="stylesheet" />
 		<link type="text/css" href="<?=cs_res_url('style/main.css');?>" rel="stylesheet" />
 		<script type="text/javascript" src="<?=cs_res_url('jqueryui/js/jquery-1.3.2.min.js');?>"></script>
 		<script type="text/javascript" src="<?=cs_res_url('jqueryui/js/jquery-ui-1.7.2.custom.min.js');?>"></script>
